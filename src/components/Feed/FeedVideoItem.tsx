369e62afce1f22a022622f7fefff7e055bbdf51b
Karamoko Israël Abdelaziz Axel <karamokoisrael@gmail.com>
2024-03-14 03:56:42 +0000

    deleting useless files

--- src/components/Feed/FeedVideoItem.tsx
+++ src/components/Feed/FeedVideoItem.tsx
@@ -2,13 +2,14 @@ import { WINDOW_HEIGHT, WINDOW_WIDTH } from "@/configurations/constants/layout";
 import { FeedVideo } from "@/core/domain/feed/feed-video.model";
 import tw from "@/styles/tailwind";
 import { Video, ResizeMode } from "expo-av";
-import { SafeAreaView, View, Image, Text } from "react-native";
+import { SafeAreaView, View, Text } from "react-native";
 import ProfileAvatar from "../Images/ProfileAvatar";
 import { generateRandomArray } from "@/libs/helpers/mock.helper";
+import { FontAwesome } from "@expo/vector-icons";
 type Props = {
   data: FeedVideo;
 };
-export default function FeedVideoItem({ data }: Props) {
+export default function FeedVideoItem({ data }: Readonly<Props>) {
   return (
     <Video
       source={{ uri: data.url }}
@@ -22,32 +23,36 @@ export default function FeedVideoItem({ data }: Props) {
       // usePoster={true}
       // posterSource={{ uri: data.poster }}
     >
-      <SafeAreaView style={tw`flex items-center z-50`}>
-        <View style={tw`flex`}>
-          {/* Card Left Side */}
-          <View style={tw`float-left flex-row items-center`}>
-            <ProfileAvatar source={{ uri: data.avatar }} />
-            <View style={tw`flex p-2 items-start justify-between`}>
-              <Text style={tw`text-base font-bold mt-5`}>
-                {data.profile.username}
-              </Text>
-              <Text style={tw`text-base font-bold mt-5`}>
-                {data.profile.subtitle}
-              </Text>
-            </View>
+      <SafeAreaView
+        style={tw`flex justify-around flex-row items-center justify-around z-50 mt-8`}
+      >
+        {/* Card Left Side */}
+        <View style={tw`flex-row items-center justify-center h-16 bg-red-100`}>
+          <ProfileAvatar style={tw`bg-red-100`} source={{ uri: data.avatar }} size={10} />
+          <View style={tw`flex pl-2 pl items-start justify-center bg-orange-100 h-full`}>
+            <Text style={tw`text-sm font-bold mt-5`}>
+              {data.profile.username}
+            </Text>
+            <Text style={tw`text-sm font-bold mt-5`}>
+              {data.profile.subtitle}
+            </Text>
           </View>
+        </View>
 
-          {/* Card Right Side */}
-          <View style={tw``}>
-            <View style={tw`flex flex-row`}>
-              {generateRandomArray(3).map((item) => (
-                <ProfileAvatar
-                  key={item}
-                  size={10}
-                  source={{ uri: data.avatar }}
-                />
-              ))}
-            </View>
+        {/* Card Right Side */}
+        <View style={tw`bg-red-100`}>
+          <View style={tw`flex flex-row`}>
+            {generateRandomArray(3).map((item) => (
+              <ProfileAvatar
+                key={item}
+                size={8}
+                source={{ uri: data.avatar }}
+              />
+            ))}
+          </View>
+          <View style={tw`flex flex-row items-center justify-around`}>
+            <Text style={tw`text-xl font-medium text-slate-900`}>174</Text>
+            <FontAwesome size={8} icon="close" />
           </View>
         </View>
       </SafeAreaView>
